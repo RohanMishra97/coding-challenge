27822c8c7458e5f5563a7282ae659667853d5724
RohanMishra97 <rohan.mishra1997@gmail.com>
2021-06-09 03:06:39 +0530

    handled corner cases, added types

--- src/funktools.py
+++ src/funktools.py
@@ -1,7 +1,18 @@
-def fold(x, func, init_val=None):
+from collections.abc import Iterable, Callable
+
+
+def fold(x: Iterable, func: Callable, init_val=None):
     it = iter(x)
-    res = init_val if init_val else next(it)
+    if init_val is None:
+        try:
+            res = next(it)
+            rtype = type(res)
+        except StopIteration:
+            raise TypeError(f"Invalid iterable: {x} & init value: {init_val}")
+    else:
+        res = init_val
+        rtype = type(init_val)
     for element in it:
         res = func(res, element)
+    assert type(res) == rtype  # sanity check
     return res
-
--- tests/test_funktools.py
+++ tests/test_funktools.py
@@ -5,12 +5,27 @@ sys.path.append(os.path.abspath("."))
 from src.funktools import fold
 
 
+def plus(a, b):
+    return a + b
+
+
+plus.__name__ = "+"
+
+
 @pytest.mark.parametrize("seq, acc, res", [
     ([i for i in range(1, 4)], 0, 6),
     (["fold", " like", " protein"], "", "fold like protein"),
     ([1, 2, 3], None, 6)
 ])
 def test_plus_fold(seq, acc, res):
-    def plus(a, b):
-        return a + b
     assert fold(seq, plus, acc) == res
+
+
+def test_non_iterable():
+    with pytest.raises(TypeError):
+        fold(None, plus, 0.0)
+
+
+def test_empty_seq_no_init():
+    with pytest.raises(TypeError):
+        fold([], plus, None)
