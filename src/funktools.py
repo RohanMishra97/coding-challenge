ae03d3ebe46a977b77a854bc27dc8a3965914ceb
RohanMishra97 <rohan.mishra1997@gmail.com>
2021-06-09 03:02:19 +0530

    handle null init_val case

--- src/funktools.py
+++ src/funktools.py
@@ -1,6 +1,7 @@
-def fold(x, func, init_val):
-    res = init_val
-    for element in x:
+def fold(x, func, init_val=None):
+    it = iter(x)
+    res = init_val if init_val else next(it)
+    for element in it:
         res = func(res, element)
     return res
 
--- tests/test_funktools.py
+++ tests/test_funktools.py
@@ -8,6 +8,7 @@ from src.funktools import fold
 @pytest.mark.parametrize("seq, acc, res", [
     ([i for i in range(1, 4)], 0, 6),
     (["fold", " like", " protein"], "", "fold like protein"),
+    ([1, 2, 3], None, 6)
 ])
 def test_plus_fold(seq, acc, res):
     def plus(a, b):
